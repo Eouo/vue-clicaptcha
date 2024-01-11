7a7a530489aaa995d27fac5794ada9ad2908ef5f
iozhaq <iozhaq@outlook.com>
2024-01-11 12:13:48 +0800

    update version

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // import VueCookies from 'vue-cookies'
 import {createApp} from 'vue'
 import App from './App.vue'
-import VueCookies from 'vue-cookies'
+
 const app = createApp(App)
 app.config.globalProperties.productionTip = false
 // Vue.use(VueCookies)
